157759689079bfa8b2e6afcbe4b61c0b715d9f53
Abhay Mathur <a21.mathur21@gmail.com>
2023-08-29 15:32:10 +0530

    added project

---
+++ my_tennis_club/members/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+# Create your views here.
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template=loader.get_template("all_members.html")
+    context={
+        'mymembers': mymembers
+    }
+    return HttpResponse(template.render(context,request))
+
+def details(request,id):
+    mymember = Member.objects.get(id=id)
+    template=loader.get_template("details.html")
+    context={
+        'mymember': mymember
+    }
+    return HttpResponse(template.render(context,request))
+
+def main(request):
+    template = loader.get_template("main.html")
+    return HttpResponse(template.render())
+
+def testing(request):
+    template = loader.get_template("template.html")
+    mymembers = Member.objects.all().values()
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context,request))
